4b60d71bb8e3ac02cde79f60a2d0a66c7850e8c7
Dave Forgac <tylerdave@tylerdave.com>
2019-04-06 23:59:46 -0400

    enable social card generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,15 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-social-cards`,
+          // ...
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,

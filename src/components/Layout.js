7a267ffbea4f4207433bda1dddce5f3368aaa6c6
Dave Forgac <tylerdave@tylerdave.com>
2019-04-06 23:23:17 -0400

    add social card headers

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,7 +13,9 @@ const TemplateWrapper = ({ children }) => (
             siteMetadata {
               title,
               description,
+              siteUrl,
             }
+            pathPrefix
           }
         }
     `}
@@ -34,7 +36,9 @@ const TemplateWrapper = ({ children }) => (
 	        <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/2019/" />
-          <meta property="og:image" content="/2019/img/og-image.jpg" />
+          <meta property="og:image" content={`${data.site.siteMetadata.siteUrl}${data.site.pathPrefix}/img/og-image.jpg`}/>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content={`${data.site.siteMetadata.siteUrl}/2019/twitter-card.jpg`}/>
         </Helmet>
         <Navbar />
         <div>{children}</div>
